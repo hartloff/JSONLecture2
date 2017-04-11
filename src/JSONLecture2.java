a243de707bc34a049370c09b2841d1ff4650973d
jessehartloff <hartloff@buffalo.edu>
2017-04-11 16:31:10 -0400

    initial commit

---
+++ src/JSONLecture2.java
@@ -0,0 +1,28 @@
+
+public class JSONLecture2 {
+
+
+    // 2 points
+    static String Q1(String json){
+        // given a JSON String that is an array in this format [5,"airplane","volunteer","nope"],
+        // that is 5 elements consisting of an int then 4 Strings, return the data at index 3 as a String.
+
+        return "";
+    }
+
+
+    // 3 points
+    static String Q2(String scores){
+        // given a JSON object containing scores in the format {"home":0,"visiting":0}, that is
+        // it has keys "home" and "visiting" with each storing an int value, return the key storing
+        // the larger int value or "tie" if the values are the same.
+
+        return "";
+    }
+
+
+    public static void main(String[] args) {
+
+    }
+
+}
